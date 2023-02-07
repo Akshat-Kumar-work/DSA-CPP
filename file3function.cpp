cbec530732d2c486d61b54462d20db6b5c0fa83d
Akshat <iamakku0.0.0.1@gmail.com>
2023-02-07 23:26:39 +0530

    7 feb 23

--- file3function.cpp
+++ file3function.cpp
@@ -10,14 +10,65 @@ return c;
 }*/
 
 
+/*max of 3 no function*/
+/*int maxNo (int a, int b, int c){
+  if( a>b && a>c){
+    return a;
+  }
+  else if (b>a && b>c)
+  {
+    return b;
+  }
+  else{
+    return c;
+  }
+
+}*/
+
+/*int sumupton(int n){
+  int sum=0;
+  for(int i=1; i<=n ;i++){
+    if(i%2==0){
+     sum = sum+i;
+    }
+  }
+  return sum;
+}*/
+
 int main (){
 
  /*  int a=5;
    int b=6;
 int c = addno(a,b); it is pass by value
 cout<<c;*/
-return 0;
-   
 
-   
+
+/*find max of 3 no.*/
+/*int a;
+cin>>a;
+int b;
+cin>>b;
+int c;
+cin>>c;
+
+
+int ans = maxNo(a,b,c);
+cout<<ans;*/
+
+
+/*sum of even no upto n*/
+/*int n;
+cin>>n;
+
+int sumis = sumupton (n);
+cout<<sumis;*/
+
+double a=21.09399;
+float b= 10.20;
+int c,d;
+c=(int)a;
+
+cout<<c;
+
+return 0;   
 }  
