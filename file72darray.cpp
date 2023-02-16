c02119bcdd379e1714c3400a2c18eb8df6d847db
Akshat <iamakku0.0.0.1@gmail.com>
2023-02-16 18:36:16 +0530

    commiting on 16 feb 23

--- file72darray.cpp
+++ file72darray.cpp
@@ -1,8 +1,88 @@
 //two dimesnsional array
 #include<iostream>
+#include<limits.h>
 using namespace std;
+int rowsum (int arr [3] [3], int row , int col){
+
+}
 int main(){
     //2d array
-    int arr [5] [6]; //5 row and 6 column
+   // int arr [5] [6]; //5 row and 6 column
+
+//    int arr[3] [3]={
+//     {1,2,3},
+//     {4,5,6},
+//     {2,4,8} 
+//    };
+
+//     //printing row 
+//     for(int i=0;i<3;i++){
+//     //printing column for each row
+//     for(int j=0;j<3;j++){
+//         cout<<arr [i] [j];
+//     }
+//     cout<<endl;
+//     }
     
+
+//print row sum
+
+// int arr [2] [2] ={  
+//     {1,2},
+//     {3,4}};
+//     int rowSum=0;
+//     for(int i=0;i<2;i++){
+//         for(int j=0;j<2;j++){
+//             rowSum+=arr[i] [j];
+//         }
+//         cout<<rowSum<<endl;
+//         rowSum=0;
+//     }
+
+
+//search element inside array
+    // int  arr [3] [3] {
+    //     {1,2,3},
+    //     {4,5,6},
+    //     {7,8,9}
+    // };
+
+    // cout<<"enter the key to search"<<" ";
+    // int key;
+    // cin>>key;
+
+    // for(int i=0;i<3;i++){
+    //     for(int j=0;j<3;j++){
+    //         if(key== arr[i] [j]){
+    //             cout<<"key is present ";
+    //         }
+    //     }
+    // }  
+
+    // cout<<"key is not present";  
+
+
+//transpose the matrix
+
+ int  arr [3] [3] {
+        {1,2,3},
+        {4,5,6},
+        {7,8,9}
+ };
+
+ for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        arr[j][i] = arr[i] [j];
+
+    }
+ }
+ 
+  for(int i=0;i<3;i++){
+    for(int j=0;j<3;j++){
+        cout<<arr[i] [j];
+    }
+
+    cout<<endl;
+ }
+
 }
