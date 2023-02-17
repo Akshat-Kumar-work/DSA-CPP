8bd2df7ff7e5acdd76bff9ef81e1c8cc19c2c2e7
Akshat <iamakku0.0.0.1@gmail.com>
2023-02-17 17:05:34 +0530

    commiting on 17 feb 23

--- file72darray.cpp
+++ file72darray.cpp
@@ -1,6 +1,7 @@
 //two dimesnsional array
 #include<iostream>
 #include<limits.h>
+#include<vector>
 using namespace std;
 int rowsum (int arr [3] [3], int row , int col){
 
@@ -64,25 +65,42 @@ int main(){
 
 //transpose the matrix
 
- int  arr [3] [3] {
-        {1,2,3},
-        {4,5,6},
-        {7,8,9}
- };
+//  int  arr [3] [3] {
+//         {1,2,3},
+//         {4,5,6},
+//         {7,8,9}
+//  };
 
- for(int i=0;i<3;i++){
-    for(int j=0;j<3;j++){
-        arr[j][i] = arr[i] [j];
+//  for(int i=0;i<3;i++){
+//     for(int j=0;j<3;j++){
+//         arr[j][i] = arr[i] [j];
 
-    }
- }
+//     }
+//  }
  
-  for(int i=0;i<3;i++){
-    for(int j=0;j<3;j++){
-        cout<<arr[i] [j];
+//   for(int i=0;i<3;i++){
+//     for(int j=0;j<3;j++){
+//         cout<<arr[i] [j];
+//     }
+
+//     cout<<endl;
+//  }
+
+//vector of vector /2d vector
+ 
+vector<vector<int> > arr;
+
+vector<int>a{1,2,3};
+vector<int>b{4,5,6};
+vector<int>c{7,8,9};
+
+arr.push_back(a);
+arr.push_back(b);
+arr.push_back(c);
+
+for(int i=0;i<arr.size();i++){
+    for(int j=0;j<arr[i].size();j++){
+        cout<<arr[i][j];
     }
-
-    cout<<endl;
- }
-
+}
 }
