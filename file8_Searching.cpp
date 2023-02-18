83cb4fe3f5c48072b79b257ecbc4aeeac7de1905
Akshat <iamakku0.0.0.1@gmail.com>
2023-02-18 10:58:45 +0530

    commiting on 18 feb 23

---
+++ file8_Searching.cpp
@@ -0,0 +1,31 @@
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+
+//linear search
+//it is used to search element linearly by traversing each and every element
+//  int arr [] = {1,2,3,4,5,6};
+//  int element =5;
+
+//  for(int i=0;i<6;i++){
+//    if(arr[i]==element){
+//         cout<<"element is present at index"<<" "<<i;
+//      }
+//  }
+    
+
+     //int arr[]={1,2,3,4,5};
+
+//  cout<<"enter no to search";
+//  int n ;
+//  cin>>n;
+//  for(int i =0; i<5;i++){
+//      if(arr[i]==n){
+//         cout<<"found";
+//              }
+//  }
+
+//binary search
+ 
+ }
